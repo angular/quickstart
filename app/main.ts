c15f1a74192dad9b64bba7a4a5ad5014cd87f3c1
Ward Bell <wardbell@hotmail.com>
2016-05-03 01:24:32 -0600

    chore: update to RC0; no unit testing yet

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,4 @@
-import {bootstrap}    from 'angular2/platform/browser';
+import {bootstrap}    from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
 
 bootstrap(AppComponent);
