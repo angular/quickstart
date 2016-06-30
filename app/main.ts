7f1685fab357ab670687c005a5cfce7e12179456
Foxandxss <foxandxss@gmail.com>
2016-07-01 01:13:11 +0200

    chore: update to RC4

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
-import {bootstrap}    from '@angular/platform-browser-dynamic';
-import {AppComponent} from './app.component';
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+
+import { AppComponent } from './app.component';
 
 bootstrap(AppComponent);
