3b7452cc444c49c139ea39523ced0468c2362c16
Filipe Silva <filipematossilva@gmail.com>
2016-09-01 05:43:41 +0100

    chore: update to rc6 (#202)
    chore: update to rc6

--- karma.conf.js
+++ karma.conf.js
@@ -30,10 +30,12 @@ module.exports = function(config) {
       // Reflect and Zone.js
       'node_modules/reflect-metadata/Reflect.js',
       'node_modules/zone.js/dist/zone.js',
+      'node_modules/zone.js/dist/long-stack-trace-zone.js',
+      'node_modules/zone.js/dist/proxy.js',
+      'node_modules/zone.js/dist/sync-test.js',
+      'node_modules/zone.js/dist/jasmine-patch.js',
       'node_modules/zone.js/dist/async-test.js',
       'node_modules/zone.js/dist/fake-async-test.js',
-      'node_modules/zone.js/dist/sync-test.js',
-      'node_modules/zone.js/dist/proxy.js',
 
       // RxJs.
       { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
