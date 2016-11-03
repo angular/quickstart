d633f290c3a9491b6e3ac976d5be285df734faa4
wada.yusuke <wada.yusuke@classmethod.jp>
2016-11-03 16:33:51 +0900

    HeroをGETするところまで実装した。この状態でtraceurが404といったエラーが出て先に進めない。

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { HeroService } from './hero.service';
+import { AppRoutingModule } from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
 import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroesComponent }  from './heroes.component';
 
-import { AppRoutingModule } from './app-routing.module';
+import { HeroService } from './hero.service';
+
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
    ],
    providers: [
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,17 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes'
+import 'rxjs/add/operator/toPromise';
+
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'http://www.yahoo.co.jp/'
+
+  constructor(private http: Http){}
+
   getHero(id: number): Promise<Hero> {
     return this.getHeroes()
       .then(heroes => heroes.find(hero => hero.id === id));
   }
 
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+    .toPromise()
+    .then(response => response.json().data as Hero[])
+    .catch(this.hadleError);
   }
+
+  private hadleError(error: any): Promise<any> {
+    console.error('An error occurd',error);
+    return Promise.reject(error.message || error);
+  }
+
   getHeroesSlowly(): Promise<Hero[]> {
     return new Promise<Hero[]>(resolve =>
       setTimeout(resolve, 2000))
