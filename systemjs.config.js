801aa94c670de6d82c461882536af19d01784aa2
Filipe Silva <filipematossilva@gmail.com>
2016-11-18 02:46:22 +0000

    chore(config): add 2.2.0 router/upgrade entry point (#285)

--- systemjs.config.js
+++ systemjs.config.js
@@ -21,6 +21,7 @@
       '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+      '@angular/router/upgrade': 'npm:@angular/router/bundles/router-upgrade.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
       '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
       '@angular/upgrade/static': 'npm:@angular/upgrade/bundles/upgrade-static.umd.js',
