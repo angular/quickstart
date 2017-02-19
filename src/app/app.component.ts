759e65dd95c284f998d5094a33c8734c2febbbe2
simonletort <simon.letort@sgcib.com>
2017-02-19 00:49:56 -0500

    finished https://angular.io/docs/ts/latest/tutorial/toh-pt1.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+export class Hero {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>{{hero.name}}
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
