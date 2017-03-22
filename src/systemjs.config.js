01ebafc4f19325b33bcfbc9831c46d3d480d118e
Jesus Rodriguez <Foxandxss@gmail.com>
2017-03-22 17:52:54 +0100

    chore: add moduleId removal loader

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -11,7 +11,7 @@
     // map tells the System loader where to look for things
     map: {
       // our app is within the app folder
-      app: 'app',
+      'app': 'app',
 
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
@@ -30,7 +30,12 @@
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
-        defaultExtension: 'js'
+        defaultExtension: 'js',
+        meta: {
+          './*.js': {
+            loader: 'systemjs-angular-loader.js'
+          }
+        }
       },
       rxjs: {
         defaultExtension: 'js'
