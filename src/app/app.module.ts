18a04ff93a796432778a72fe9a437e4ac54cd835
Tsvetomir <cvetomirconev@gmail.com>
2017-04-14 13:31:17 +0300

    feat: add Kendo UI buttons package and SystemJS config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports: [ BrowserModule, ButtonsModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
