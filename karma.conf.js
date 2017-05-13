1b2c9410659a4dfab12db464b8df4128b198bb1c
Adrian Nicos <adrian.nicos@gmail.com>
2017-05-13 13:15:38 +0200

    running on firefox

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
 
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter')
     ],
 
@@ -91,7 +91,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Firefox'],
     singleRun: false
   })
 }
