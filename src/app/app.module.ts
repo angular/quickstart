1174bf80458a47abf001c2efc837269a431e9db4
Thean Hoogenboezem <theunis.hoogenboezem@standardbank.co.za>
2017-12-14 10:13:20 +0200

    Added rest of changes required for Node Todo example app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
 
+//Decorator
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, HttpModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -30,6 +30,7 @@
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
+        main: './main.js',
         defaultExtension: 'js',
         meta: {
           './*.js': {
